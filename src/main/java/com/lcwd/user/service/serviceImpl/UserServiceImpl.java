932079cb6e751cba35c054f714651c02fe6e374c
amit15091996 <amitdewangan199@gmail.com>
2022-11-21 18:06:33 +0530

    first commit

---
+++ src/main/java/com/lcwd/user/service/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,59 @@
+package com.lcwd.user.service.serviceImpl;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lcwd.user.service.entity.User;
+import com.lcwd.user.service.exception.ResourceNotFoundException;
+import com.lcwd.user.service.repository.UserRepository;
+import com.lcwd.user.service.services.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public User saveUser(User user) {
+
+		// generate unique userid
+		String randomUserId = UUID.randomUUID().toString();
+		user.setUserId(randomUserId);
+		return userRepository.save(user);
+	}
+
+	@Override
+	public List<User> getAllUser() {
+
+		return userRepository.findAll();
+	}
+
+	@Override
+	public User getUser(String userId) {
+		return userRepository.findById(userId).orElseThrow(
+				() -> new ResourceNotFoundException("User with given id not found on server !! : " + userId));
+	}
+
+	@Override
+	public User updateUser(String userId) {
+		User user = userRepository.findById(userId).orElseThrow(
+				() -> new ResourceNotFoundException("User with given id not found on server !! : " + userId));
+		user.setEmail(user.getName());
+		user.setEmail(user.getEmail());
+		user.setAbout(user.getAbout());
+
+		return userRepository.save(user);
+
+	}
+
+	@Override
+	public void deleteUser(String userId) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
