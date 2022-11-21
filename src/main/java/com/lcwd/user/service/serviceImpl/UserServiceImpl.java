f5b75397c36dea66e00e21d13ae1d1af152b973d
amit15091996 <amitdewangan199@gmail.com>
2022-11-21 18:10:11 +0530

    changes

--- src/main/java/com/lcwd/user/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/lcwd/user/service/serviceImpl/UserServiceImpl.java
@@ -25,7 +25,7 @@ public class UserServiceImpl implements UserService {
 		user.setUserId(randomUserId);
 		return userRepository.save(user);
 	}
-
+ 
 	@Override
 	public List<User> getAllUser() {
 
