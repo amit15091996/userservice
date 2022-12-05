f249e98cf7122b48a1f118501315ce54dc0430c2
Amit Dewangan <amitdewangan199@gmail.com>
2022-12-05 12:46:58 +0530

    implemented feignClient

--- src/main/java/com/lcwd/user/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/lcwd/user/service/serviceImpl/UserServiceImpl.java
@@ -1,13 +1,24 @@
 package com.lcwd.user.service.serviceImpl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.lcwd.user.service.entity.Hotel;
+import com.lcwd.user.service.entity.Rating;
 import com.lcwd.user.service.entity.User;
 import com.lcwd.user.service.exception.ResourceNotFoundException;
+import com.lcwd.user.service.external.services.HotelService;
 import com.lcwd.user.service.repository.UserRepository;
 import com.lcwd.user.service.services.UserService;
 
@@ -17,6 +28,14 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	private HotelService hotelService;
+
+	private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
 	@Override
 	public User saveUser(User user) {
 
@@ -25,7 +44,7 @@ public class UserServiceImpl implements UserService {
 		user.setUserId(randomUserId);
 		return userRepository.save(user);
 	}
- 
+
 	@Override
 	public List<User> getAllUser() {
 
@@ -34,8 +53,37 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public User getUser(String userId) {
-		return userRepository.findById(userId).orElseThrow(
+		User user = userRepository.findById(userId).orElseThrow(
 				() -> new ResourceNotFoundException("User with given id not found on server !! : " + userId));
+		Rating[] ratingsOfUser = restTemplate
+				.getForObject("http://RATING-SERVICE/rating/users/" + user.getUserId(), Rating[].class);
+		
+		List<Rating> ratings = Arrays.stream(ratingsOfUser).toList();
+		
+		logger.info("-----------------" + ratingsOfUser);
+
+		List<Rating> ratingList = ratings.stream().map(rating -> {
+			// api call to hotel service to get hotel
+			// http://localhost:4040/hotels/cc80f4c3-0332-4d20-9636-a0716962269d
+
+//			ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://HOTEL-SERVICE/hotels/" + rating.getHotelId(), Hotel.class);
+			
+			
+
+			Hotel hotel = hotelService.getHotel(rating.getHotelId());
+
+//			logger.info("respone status code {} : " + forEntity.getStatusCode());
+
+			// set the hotel to rating
+
+			rating.setHotel(hotel);
+
+			// return the rating
+			return rating;
+		}).collect(Collectors.toList());
+
+		user.setRatings(ratingList);
+		return user;
 	}
 
 	@Override
